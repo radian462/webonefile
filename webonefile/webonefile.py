807d6154d3a0b0ca2ca86f118d29f0c5f725995d
radian462 <102207047+radian462@users.noreply.github.com>
2024-11-29 14:08:33 +0000

    jsとcssをダウンロードできるように

--- webonefile/webonefile.py
+++ webonefile/webonefile.py
@@ -1,4 +1,5 @@
 import base64
+from logging import getLogger, StreamHandler, Formatter, DEBUG, INFO
 from urllib.parse import urlparse
 
 import requests
@@ -6,17 +7,41 @@ from bs4 import BeautifulSoup
 
 
 def webonefile(url: str, headers: dict = {}, proxies: dict = {}) -> str:
+    logger = getLogger("Webonefile")
+    handler = StreamHandler()
+    logger.setLevel(INFO)
+    formatter = Formatter("[%(levelname)s] %(message)s - %(asctime)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    base_url = urlparse(url).scheme + "://" + urlparse(url).netloc
+
     r = requests.get(url, headers=headers)
     soup = BeautifulSoup(r.text, "html.parser")
     
     for src in soup.find_all(src=True):
         if src['src']:
             src_parsed = urlparse(src['src'])
-            if (src_parsed.hostname and src_parsed.scheme) or src_parsed.path:
-                print(src.name)
-                
-    
+            if (src_parsed.scheme and src_parsed.netloc) or src_parsed.path:
+                if src_parsed.scheme and src_parsed.netloc:
+                    src_url = src['src']
+                elif src['src'].startswith("//"):
+                    final_url = requests.head(url, allow_redirects=True).url
+                    scr_scheme = 'https' if final_url.startswith('https://') else 'http'
+                    src_url = scr_scheme + "://" + src['src'].replace("//","")
+                else:
+                    src_url = base_url + src['src']
 
+                logger.info(f"Downloading {src_url}")
+                if src.name == "script" or src.name == "style": 
+                    src_r = requests.get(src_url, headers=headers)
+                    del src['src']
+                    src.string = src_r.text
+
+    with open('test.html', 'w', encoding='utf-8') as file:
+        file.write(soup.prettify())
+
+            
 if __name__ == "__main__":
     webonefile(
         "http://web.archive.org/web/20241118000048/https://google.com/",
